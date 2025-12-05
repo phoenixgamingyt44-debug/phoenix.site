8f8c064eebe9f2c4d74baa2ff89acb8a0f3be233
phoenixgamingyt44-debug <phoenix.gaming.yt44@gmail.com>
2025-12-05 15:47:46 +1100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import PhoenixWebsite from './PhoenixWebsite.jsx'
+
+createRoot(document.getElementById('root')).render(<PhoenixWebsite />)
